c5735657784bec65a4ff550004f6ae4769525ae7
Preetikaralatti <149783950+Preetikaralatti@users.noreply.github.com>
2024-01-20 14:02:10 +0000

    data_types

---
+++ .devcontainer/data_types.js
@@ -0,0 +1,18 @@
+console.log("Preeti");
+
+let name="Preeti"
+let age=18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+//boolean =>true/false
+//null => standalone value
+// undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof"Preeti");
+console.log(typeof null);// null is an object 
