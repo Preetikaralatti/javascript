4340155e8ec176cb0d476688aaba68d0cdaf766a
Preetikaralatti <149783950+Preetikaralatti@users.noreply.github.com>
2024-01-21 12:59:59 +0000

    conversion

--- .devcontainer/conversiononOperation.js
+++ .devcontainer/conversiononOperation.js
@@ -5,3 +5,26 @@ console.log(typeof(score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
+
+//******************Operetion****************
+
+let value= 33
+let negValue = -33
+console.log(negValue);
+
+console.log(2**3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+let gameCounter= 100
+gameCounter++
+console.log(gameCounter);
