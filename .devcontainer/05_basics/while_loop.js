be7d2cca0a476c2f199e93bbc055c115ecf22a06
Preetikaralatti <149783950+Preetikaralatti@users.noreply.github.com>
2024-02-05 17:10:45 +0000

    while_loop

---
+++ .devcontainer/05_basics/while_loop.js
@@ -0,0 +1,21 @@
+// while loop
+let i=0
+while (i<=10) {
+    console.log(`value of index is ${i}`);
+    i=i+2
+}
+
+let MyArray=["flash","batman","superman"]
+let arr=0
+while (arr < MyArray.length) {
+    console.log(`value is ${MyArray[arr]}`);
+    arr=arr+1
+}
+
+// do while
+
+let score=11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
